a96cb8d33e50af1e2957f8727e6ebbd171d01810
listsarah <sarah.listzwan@gmail.com>
2024-09-03 18:31:15 -0400

    untested board setup partially done

--- chess.cpp
+++ chess.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
 #include <chess.h>
 
-u_int64_t Board;
+Chessboard::Chessboard(u_int64_t* board, u_int8_t peice, bool color){
+    Chessboard.board = board;
+    Chessboard.peice = peice;
+    Chessboard.color = color;
+}
+
+Chessboard::setup(u_int8_t type, bool color){
+    switch(type){
+        case 0x00:
+            board = 
+    }
+}
